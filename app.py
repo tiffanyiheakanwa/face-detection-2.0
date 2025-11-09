1bf7a145fad924fa1d867f526cd6cfc02bebb60d
tiffanyiheakanwa <tiffanyiheakanwa@gmail.com>
2025-11-09 15:52:17 +0100

    deployment

--- app.py
+++ app.py
@@ -269,5 +269,5 @@ def stats():
 
 if __name__ == '__main__':
     # Run the app
-    # Debug=True for development, set to False for production
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
